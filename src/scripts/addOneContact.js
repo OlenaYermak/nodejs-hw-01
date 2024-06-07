7c98add9cdf5ae8a9be3e93a20658366c606c595
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-06-07 19:32:44 +0300

    add project

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    const newContact = createFakeContact();
+
+    contacts.push(newContact);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+    console.log('Дані успішно додані');
+  } catch (error) {
+    console.error('Помилка додавання данних', error);
+  }
+};
 
 await addOneContact();
--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+
+    const remainingContacts = contacts.filter(() => Math.random() > 0.5);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(remainingContacts, null, 2));
+    console.log('Данні успішно видалені');
+  } catch (error) {
+    console.error('Помилка', error);
+  }
+};
 
 await thanos();
